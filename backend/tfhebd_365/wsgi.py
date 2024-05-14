1cded933c01917256c6f90995345fb6464def3c2
Crowdbotics <engineering@crowdbotics.com>
2024-05-14 06:06:00 +0000

    Initial commit

---
+++ backend/tfhebd_365/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for tfhebd_365 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "tfhebd_365.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tfhebd-365",
+  url: "https://tfhebd-365.botics.co",
+  api: "https://tfhebd-365.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
